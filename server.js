b6828775416ab575379748fcbdb3cb2338ff1fde
tzvimandelbaum <tzvilab@gmail.com>
2026-01-18 03:03:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require("path");
 
 const app = express();
 
-// Serve files from /public
+// Serve everything from /public
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About page ✅</h1><a href='/'>Back</a>");
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
